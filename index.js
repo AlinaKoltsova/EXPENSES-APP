88e746f9ed8d2ccac56b433400e8c8273eb07544
Koltsova Alina <6-alisa@mail.ru>
2024-01-26 12:07:00 +0600

    add expenses list

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ const expenses = [];
 
 const inputExpensesNode = document.querySelector ('.js-expense-input');
 const buttonExpensesNode = document.querySelector ('.js-expense-btn');
+const espenseHistoryNode = document.querySelector ('.js-expenses-history');
+const sumExpensesTotal = document.querySelector ('.js-sum-total');
+
 
 buttonExpensesNode.addEventListener('click', function () {
+//получаем значение из поля ввода
+    if (!inputExpensesNode.value) {
 
-    const expense = inputExpensesNode.value;
+        return;
+    }
+    
+    const expense = parseInt (inputExpensesNode.value);
 
+    inputExpensesNode.value = '';
+
+//сохраняем значение в поле ввода
     expenses.push (expense);
 
-    console.log (expenses);
+//выводим список трат
 
+    let expensesListHtml ='';
+
+    expenses.forEach(element => {
+        expensesListHtml += `<li>${element}</li>`;
+    });
+    espenseHistoryNode.innerHTML = `<ol>${expensesListHtml}</ol>`;
+
+    //считаем сумму 
+
+    let sumTotal = 0;
+    expenses.forEach(element => {
+        sumTotal += element;
+    });
+
+    sumExpensesTotal.innerText = sumTotal;
 });
