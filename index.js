bc86931aa25d0694cf90c3761004bacd02107576
Koltsova Alina <6-alisa@mail.ru>
2024-01-29 12:18:52 +0600

    fix sum

--- index.js
+++ index.js
@@ -1,40 +1,104 @@
-const expenses = []; 
+const LIMIT = 100000;
+const CURRENCY = ' руб.';
+const STATUSINLIMIT = 'все хорошо';
+const STATUSOUTLIMIT = 'все плохо';
+const STATUSOUTLIMIT_CLASSNAME = 'limit-status_red';
 
 const inputExpensesNode = document.querySelector ('.js-expense-input');
 const buttonExpensesNode = document.querySelector ('.js-expense-btn');
 const espenseHistoryNode = document.querySelector ('.js-expenses-history');
-const sumExpensesTotal = document.querySelector ('.js-sum-total');
+const sumExpensesTotalNode = document.querySelector ('.js-sum-total');
+const expenseLimitNode = document.querySelector ('.js-limit');
+const expenseLimitStatusNode = document.querySelector ('.js-limit-status');
 
+const expenses = [];
+
+init (expenses);
 
 buttonExpensesNode.addEventListener('click', function () {
-//получаем значение из поля ввода
-    if (!inputExpensesNode.value) {
+
+    const expense = getExpensefromuser();
+
+    if (!expense) {
 
         return;
     }
     
+    trackExpenses (expense);
+    render (expenses);
+});
+
+function init (expenses) {
+
+    expenseLimitNode.innerText = LIMIT;
+    expenseLimitStatusNode.innerText = STATUSINLIMIT;
+    
+};
+
+function getExpensefromuser () {
+
+    if (!inputExpensesNode.value) {
+        return null;
+    }
     const expense = parseInt (inputExpensesNode.value);
+    clearInput ();
+    return expense;
+};
 
-    inputExpensesNode.value = '';
+function trackExpenses (expense) {
 
-//сохраняем значение в поле ввода
     expenses.push (expense);
+};
 
-//выводим список трат
-
-    let expensesListHtml ='';
-
-    expenses.forEach(element => {
-        expensesListHtml += `<li>${element}</li>`;
-    });
-    espenseHistoryNode.innerHTML = `<ol>${expensesListHtml}</ol>`;
-
-    //считаем сумму 
+function calculateExpanses (expenses) {
 
     let sumTotal = 0;
+
     expenses.forEach(element => {
         sumTotal += element;
     });
 
-    sumExpensesTotal.innerText = sumTotal;
-});
+    return sumTotal;
+};
+
+function renderHistory (expenses) {
+
+    let expensesListHtml ='';
+
+    expenses.forEach(element => {
+        expensesListHtml += `<li>${element}${CURRENCY}</li>`;
+    });
+
+    espenseHistoryNode.innerHTML = `<ol>${expensesListHtml} </ol>`;
+};
+
+function renderStatusExpenses (sumTotal) {
+
+    if (sumTotal<= LIMIT) {
+
+        expenseLimitStatusNode.innerText = STATUSINLIMIT;
+     } else {
+
+        expenseLimitStatusNode.innerText = STATUSOUTLIMIT;
+        expenseLimitStatusNode.classList.add (STATUSOUTLIMIT_CLASSNAME);
+     }
+};
+
+function renderSumTotal (sumTotal) {
+
+    sumExpensesTotalNode.innerText = sumTotal;
+};
+
+function render (expenses) {
+
+    const sumTotal = calculateExpanses (expenses);
+
+    renderSumTotal (sumTotal);
+    renderHistory (expenses);
+    renderStatusExpenses (sumTotal);
+};
+
+function clearInput () {
+
+    inputExpensesNode.value = '';
+};
